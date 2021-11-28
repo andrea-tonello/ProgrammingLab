cb959369f4921b7df9a31700541f023a1fed968d
andrea-tonello <91379085+andrea-tonello@users.noreply.github.com>
2021-11-28 21:43:50 +0000

    Esercizio 3 finito

---
+++ Esercizio3.py
@@ -0,0 +1,46 @@
+class CSVFile():
+    
+    def __init__(self,name,file):
+        self.nome = name
+        self.file = file
+
+    def get_data(self):
+        valori = []
+        file = open(self.file,'r')
+        for line in file:
+            valori.append(line)
+        return valori
+        file.close()
+    
+    def get_date_vendite(self):
+        dates = []
+        file = open(self.file, 'r')
+        for line in file:
+            elements = line.split(',')
+            dates.append(elements[0])
+        return dates
+        file.close()
+
+    def __str__(self):
+        file = open(self.file, 'r')
+        file_contents = file.read()
+        header = file_contents[0:11]
+        return header
+        file.close()
+
+mio_oggetto = CSVFile('Vendite', 'shampoo_sales.csv')
+
+valori = mio_oggetto.get_data()
+for line in valori:
+    print(line)
+
+dates = mio_oggetto.get_date_vendite()
+for line in dates:
+    print(line)
+
+print("\n")
+
+header = mio_oggetto.__str__()
+print(header)
+
+
--- saluti.py
+++ saluti.py
@@ -1,18 +1,1 @@
-def somma_vettoriale(lista, lista2):
-    final_list = []
-    for item in lista or lista2:
-        if type(item)!=int:
-            print("Le liste accettano solo valori interi")
-            return final_list
-            exit()
-    if len(lista)==len(lista2):
-        final_list = sum(lista) + sum(lista2)
-    else:
-        print("Le liste non sono della stessa lunghezza")
-    
-    return final_list
-    
-
-my_list = [6,4,3,1.4]
-my_list2 = [7,8,9,0]
-print("La somma vettoriale è {}".format(somma_vettoriale(my_list,my_list2)))
+file = open('shampoo_sales.csv', 'r')
