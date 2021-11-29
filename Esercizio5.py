19923b16d0c83c3118b8b9285a839d8c4d2df587
andrea-tonello <91379085+andrea-tonello@users.noreply.github.com>
2021-11-29 21:49:32 +0000

    Esercizio 5 finito

--- Esercizio5.py
+++ Esercizio5.py
@@ -9,7 +9,7 @@ class Automobile():
         return "Produttore: {}\nModello: {}\nNumero posti: {}\nTarga: {}".format(self.casa_automo, self.modello, self.numero_posti, self.targa)
 
     def parla(self):
-        print("Broom Broom")
+        print("\"Broom Broom\"")
 
     def confronta(self, auto2):
         if(self.casa_automo==auto2.casa_automo and self.modello==auto2.modello and self.numero_posti==auto2.numero_posti):
@@ -19,15 +19,21 @@ class Automobile():
 
 class Transformer(Automobile):
     def __init__(self, nome, generazione, grado, schieramento):
-        self.nome = nome
-        self.generazione = generazione
-        self.grado = grado
-        self.schieramento = schieramento
+        self.casa_automo = nome
+        self.modello = generazione
+        self.numero_posti = grado
+        self.targa = schieramento
     
     def scheda_militare(self):
-        return "Caratteristiche tecniche: {}, {}, {}, {}".format(self.nome, self.generazione, self.grado, self.schieramento)
+        return "Nome Transformer: {}\nGenerazione: {}\nGrado militare: {}\nFazione: {}".format(self.casa_automo, self.modello, self.numero_posti, self.targa)
+    
+    def confronta(self, transformerB):
+        if(self.casa_automo==transformerB.casa_automo and self.modello==transformerB.modello and self.numero_posti==transformerB.numero_posti):
+            print("I due transformers sono uguali, perciò")
+        else:
+            print("I due transformers sono diversi, perciò")
+        super().confronta(transformerB)
         
-
 auto1 = Automobile('Alfa Romeo', '33 Stradale', '2', 'PN 256 IT')
 auto2 = Automobile('Alfa Romeo', '33 Stradale', '2', 'TS 789 IT')
 auto3 = Automobile('BMW', 'M5', '4', 'RM 384 IT')
@@ -39,9 +45,22 @@ auto1.parla()
 auto1.confronta(auto2)
 auto1.confronta(auto3)
 
-print("\n")
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+print()
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-transformerAlfa = Transformer('Optimus Prime', 'Gen 1', 'Capitano', 'Autobot')
-print(transformerAlfa.scheda_militare())
+transformer1 = Transformer('Optimus Prime', 'Gen 1', 'Capitano', 'Autobot')
+transformer2 = Transformer('Optimus Prime', 'Gen 1', 'Capitano', 'Autobots_')
+transformer3 = Transformer('Grimlock', 'Gen 1', 'Leader', 'Dinobot')
+
+print(transformer1.scheda_militare())
+
+transformer1.parla()
+
+transformer1.confronta(transformer2)
+transformer1.confronta(transformer3)
+
+
+
 
 
